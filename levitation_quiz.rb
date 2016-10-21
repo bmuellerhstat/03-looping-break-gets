5ad865a12d1a499816622548cce7c896ac3e0557
Gabriel Charriez <gabrielc0464@hstat.org>
2016-10-21 22:42:30 +0000

    late hw

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,9 @@
 def levitation_quiz
-	#your code here
-	
+ 	
+loop do 
+    puts "What is the spell that enacts levitation?"
+    a = gets.chomp
+ break if a == "Wingardium Leviosa"
 end
+    puts "You passed the quiz!"
+  	end 
