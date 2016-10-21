b25b8ec1cb131b3408c30b86a7d5210682a2f1a2
justing5321 <justing5321@hstat.org>
2016-10-21 02:06:22 +0000

    finish hwk

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,11 @@
 def levitation_quiz
-	#your code here
+	loop do
+	    
+	puts "What is the spell that enacts levitation?"
+    answer = gets.chomp #gets user input
+	    break if answer == "Wingardium Leviosa" #only if answer is < then the test will stop
+	    
+	end
+	puts "You passed the quiz!"
 	
 end
