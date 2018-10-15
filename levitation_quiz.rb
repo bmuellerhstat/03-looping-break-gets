c5ced1263918d0b8933ea17c14b21dd241bbbbf4
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-10-15 17:45:31 +0000

    hw levitation_quiz.rb

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,9 @@
 def levitation_quiz
 	#your code here
-	
+    spell = ""
+	until spell == "Wingardium Leviosa"
+	    puts "What is the spell that enacts levitation?"
+	    spell = gets.chomp
+	end
+	puts "You passed the quiz!"
 end
