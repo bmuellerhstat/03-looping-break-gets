83f793130c00e4cfc7575077e47140c246b747c9
Ahmad Rabah <ahmadr9279@hstat.org>
2018-10-15 17:10:38 +0000

    Make a question looped until it meets a condition

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,13 @@
 def levitation_quiz
-	#your code here
+	puts "What is the spell that enacts levitation?"
+	magic_word = gets.chomp
+	until magic_word == "Wingardium Leviosa"
+	puts "What is the spell that enacts levitation?"
+	magic_word = gets.chomp
+	end
+	if magic_word == "Wingardium Leviosa"
+	    puts "You passed the quiz!"
+	else
+	end
 	
 end
