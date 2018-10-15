70de02255923aa5c15046349cd14acf563fe118f
Hector Marin <hectorm6066@hstat.org>
2018-10-15 20:06:13 +0000

    Finished

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,9 @@
 def levitation_quiz
 	#your code here
-	
+	user_input =" "
+	    until user_input =="Wingardium Leviosa"
+	    puts "What is the spell that enacts levitation?"
+	    user_input =gets.chomp
+	    end
+	puts "You passed the quiz!"
 end
