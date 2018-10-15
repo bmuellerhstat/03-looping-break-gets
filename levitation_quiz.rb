f3dbdbf976f2e0304e7dc06343114558fd08083c
Raquel Joseph <raquelj6437@hstat.org>
2018-10-15 16:48:46 +0000

    pass rspec

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,9 @@
 def levitation_quiz
 	#your code here
-	
+	words = ''
+	while words != "Wingardium Leviosa"
+    	puts 'What is the spell that enacts levitation?'
+    	words = gets.chomp.to_s
+	end
+	puts "You passed the quiz!"
 end
