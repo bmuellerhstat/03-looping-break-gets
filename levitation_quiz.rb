ba7636bd2e544016b2b725d68f619a7725a5e622
Travis Matos <travism7618@hstat.org>
2018-10-16 13:43:21 +0000

    fixes levitation quiz method

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,10 @@
 def levitation_quiz
 	#your code here
-	
+	loop{
+	    puts "What is the spell that enacts levitation?"
+	    answer=gets.chomp
+	    break if answer=="Wingardium Leviosa"
+	}
+	puts "You passed the quiz!"
 end
+
