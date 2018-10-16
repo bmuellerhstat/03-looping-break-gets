cdc501c6324f114da8a86174e2dccc5ea3044c6a
Toby lau <yatchol6214@hstat.org>
2018-10-16 07:58:39 +0000

    finish hw

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,11 @@
 def levitation_quiz
-	#your code here
+	puts 'What is the spell that enacts levitation?'
+	input=gets.chomp
 	
+	until input=='ingardium Leviosa'
+	puts'You passed the quiz!'
+	break
+	end
 end
+
+levitation_quiz
