115e666c1ec4b69564bd9acc4adfc78bff80a253
Donia Abdelhalim <doniaa3710@hstat.org>
2018-10-16 02:35:59 +0000

    add loop

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,11 @@
 def levitation_quiz
-	#your code here
-	
+	#your code here. 
+#	it contains a loop that asks the user "What is the spell that enacts levitation?" It should then store the answer in a variable called answer using the gets.chomp method. The loop should break if the user's answer is "Wingardium Leviosa". When the loop breaks, our method should puts "You passed the quiz!" Otherwise, the loop should continue.
+loop do
+	puts "What is the spell that enacts levitation?"
+	answer = gets.chomp
+	break if answer == "Wingardium Leviosa"
 end
+puts "You passed the quiz!"
+end
+levitation_quiz
