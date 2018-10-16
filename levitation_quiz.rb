9f350de94c4501d40017b4d7e6ed321ce6cac45e
Anabel Arbeeny <anabela0701@hstat.org>
2018-10-16 01:18:26 +0000

    code test

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,14 @@
 def levitation_quiz
 	#your code here
-	
+	loop do 
+	    quiz= "What is the spell that enacts levitation?"
+	    answer = gets.chomp
+	    until answer == "Wingardium Leviosa"
+	        puts quiz
+	    end 
+	    if answer == "Wingardium Leviosa"
+	    	puts"You passed the quiz!"
+	    	break 
+	    end 
+	end 
 end
